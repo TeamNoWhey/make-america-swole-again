1aba967577c90901b70ceda27c7b74b77175d7f3
Rachel <racheldepriest1@gmail.com>
2017-01-25 10:32:32 -0600

    Now sending correct index.html

--- server/routes/main.js
+++ server/routes/main.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 router.get('/', function (req, res, next){
   //res.send('hello');
-  res.sendFile(path.join(__dirname + '/../../index.html'));
+  res.sendFile(path.join(__dirname + '/../../client/index.html'));
 })
 
 module.exports = router;
